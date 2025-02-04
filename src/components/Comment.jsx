bc1028c69358450973cf89764b11cede634e572d
Gabriel Lima <gabriiel_sl@hotmail.com>
2025-02-04 18:00:56 -0300

    first commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+
+export function Comment({ content }) {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/gabriielsl.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabriel Santos</strong>
+                            <time title="24 de Janeiro às 18:15" dateTime="2025-01-24 18:15:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
