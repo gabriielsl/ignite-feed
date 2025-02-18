b681792c2053ba723a15b09fd6de502ecc12d368
Gabriel Lima <gabriiel_sl@hotmail.com>
2025-02-18 19:07:20 -0300

    Extensão de interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Header } from "./components/Header.tsx"
-import { Post } from "./components/Post.tsx"
+import { Post, PostType } from "./components/Post.tsx"
 import { Sidebar } from "./components/Sidebar.tsx"
 
 import styles from './App.module.css'
 
 import './global.css'
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -49,9 +49,7 @@ export function App() {
             return (
               <Post 
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           }) }
