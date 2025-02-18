54b4dd995c2968556551a085def75ed37a55c778
Gabriel Lima <gabriiel_sl@hotmail.com>
2025-02-18 18:34:28 -0300

    Corrigindo erros com TypeScript

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,14 +1,30 @@
 import { format, formatDistanceToNow } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
-import { useState } from 'react'
+import { ptBR } from 'date-fns/locale/pt-BR'
+import { FormEvent, useState, ChangeEvent, InvalidEvent } from 'react'
 
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
 
+interface Author {
+    name: string
+    role: string
+    avatarUrl: string
+}
 
-export function Post({ author, publishedAt, content }) {
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string
+}
+
+interface PostProps {
+    author: Author
+    publishedAt: Date
+    content: Content[]
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
     const [comments, setComments] = useState([
         'Post muito bacana hein?!'
     ])
@@ -23,23 +39,23 @@ export function Post({ author, publishedAt, content }) {
         addSuffix: true
     })
 
-    function handleCreateNewComment() {
+    function handleCreateNewComment(event: FormEvent) {
         event.preventDefault()
 
         setComments([...comments, newCommentText])
         setNewCommentText('')
     }
 
-    function handleNewCommentChange() {
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('')
         setNewCommentText(event.target.value)
     }
 
-    function handleNewCommentInvalid() {
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('Esse campo é obrigatório!')
     }
 
-    function deleteComment(commentToDelete) {
+    function deleteComment(commentToDelete: string) {
         const commentsWithoutDeletedOne = comments.filter(comment => {
             return comment !== commentToDelete
         })
